671d5ecc754ad3c224621cbb915aca2e65e87865
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-03 00:04:48 -0400

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import './globals.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
   )
 }
 
