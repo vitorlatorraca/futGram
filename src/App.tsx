2bda1a50dbf13e13163770a9235a7f4c878c1771
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-03 00:18:20 -0400

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './globals.css';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <main className="flex h-screen">
+    <Routes>
+      { /*public routes */ }
+    <Route path="/sign-in" element={<SigninForm />} />
+
+
+      {  /*public routes */}
+      <Route index element={<Home />} />
+    </Routes>
+  </main>
 
   )
 }
