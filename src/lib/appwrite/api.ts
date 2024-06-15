badeb662a7cbe9d34e7afbbb40b40cb713f69692
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-15 01:19:43 -0400

    new

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,19 +1,19 @@
-import { ID } from 'appwrite'
-
+import { ID } from 'appwrite';
 import { INewUser } from "@/types";
+import { account } from './config';
 
 export async function createUserAccount(user: INewUser) {
-    try{
+    try {
         const newAccount = await account.create(
             ID.unique(),
             user.email, 
             user.password,
             user.name 
-        )
-
-    } catch (error){
-        console.log(error);
-        return error;
+        );
+        // Retornar o novo usuário criado
+        return newAccount;
+    } catch (error) {
+        console.error("Failed to create user account:", error);
+        throw error;  // Lançar o erro para ser tratado na camada de chamada
     }
-
 }
