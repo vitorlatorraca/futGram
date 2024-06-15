09be2d87713fb5a199e1c5865e2f5a658d85dadf
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-15 01:03:50 -0400

    new

--- src/lib/appwrite/config.js
+++ src/lib/appwrite/config.js
@@ -2,6 +2,7 @@ import { Client, Account, Databases, Storage, Avatars } from 'appwrite'
 
 export const appwriteConfig = {
     projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    url: import.meta.env.VITE_APPWRITE_URL,
 }
 
 export const client = new Client();
